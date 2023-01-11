3750e45c676a074dc3b8881615365520a98ee2c5
Albrecht Lorin <lorin.albrecht@buhlergroup.com>
2023-01-11 11:18:37 +0100

    Changed Structure of the Program (Functions)

--- Calculator.c
+++ Calculator.c
@@ -10,28 +10,8 @@ Update              11. January 2022
 // Include of Functions
 #include <stdio.h>
 
-// Function to enter value
-double enter_value(short round) // Parameter Round
-{
-    // Declaration of Variable
-    double value = 0; // value as double
-    int ret = 0; // ret to Check if the Enter is a number
-    // do while loop to Check if the Enter is a number
-    do
-    {
-        printf("Enter Your %d. Value: ", round);
-        ret = scanf("%lf", &value);
-        fflush(stdin);
-        // Checking if ret is not equal to 1
-        if (ret != 1)
-        {
-            printf("Your Enter is Invalid. Try again\n\n");
-        }
-    } while (ret != 1);
-
-    // return input is value (double)
-    return value;
-}
+// Declaration of Functions
+double enter_value(short round);
 
 // Main Function
 int main()
@@ -121,4 +101,27 @@ int main()
 
     // Return Value 0 for Main Function
     return 0;
+}
+
+// Function to enter value
+double enter_value(short round) // Parameter Round
+{
+    // Declaration of Variable
+    double value = 0; // value as double
+    int ret = 0; // ret to Check if the Enter is a number
+    // do while loop to Check if the Enter is a number
+    do
+    {
+        printf("Enter Your %d. Value: ", round);
+        ret = scanf("%lf", &value);
+        fflush(stdin);
+        // Checking if ret is not equal to 1
+        if (ret != 1)
+        {
+            printf("Your Enter is Invalid. Try again\n\n");
+        }
+    } while (ret != 1);
+
+    // return input is value (double)
+    return value;
 }
